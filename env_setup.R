ba36df04b7a58dea532b8340defa77db9d5111ff
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-12-13 12:36:33 -0500

    Update env_setup.R

--- env_setup.R
+++ env_setup.R
@@ -25,43 +25,95 @@ measurement          <- read_csv(file.path(data_dir, "measurement.csv"))
 visit_occurrence     <- read_csv(file.path(data_dir, "visit_occurrence.csv"))
 drug_exposure        <- read_csv(file.path(data_dir, "drug_exposure.csv"))
 observation          <- read_csv(file.path(data_dir, "observation.csv"))
-concept              <- read_csv(file.path(data_dir, "2b_concept.csv"))
-concept_relationship <- read_csv(file.path(data_dir, "2b_concept_relationship.csv"))
 
 # ============================
-# 3. Example transformation
+# 3. Create usable concept tables
 # ============================
 
-# Unique condition concept IDs
+
+# Unique concept IDs
 cond_concepts <- condition_occurrence %>%
   select(condition_concept_id) %>%
   mutate(condition_concept_id = as.integer(condition_concept_id)) %>%
+  rename(concept_id = condition_concept_id) %>%
   distinct() %>%
   tidyr::drop_na()
 
+rm(condition_occurrence)
+
+proc_concepts <- procedure_occurrence %>%
+  select(procedure_concept_id) %>%
+  mutate(procedure_concept_id = as.integer(procedure_concept_id)) %>%
+  rename(concept_id = procedure_concept_id) %>%
+  distinct() %>%
+  tidyr::drop_na()
+
+rm(procedure_occurrence)
+
+drug_concepts <- drug_exposure %>%
+  select(drug_concept_id) %>%
+  mutate(drug_concept_id = as.integer(drug_concept_id)) %>%
+  rename(concept_id = drug_concept_id) %>%
+  distinct() %>%
+  tidyr::drop_na()
+
+rm(drug_exposure)
+
+observation_concepts <- observation %>%
+  select(observation_concept_id) %>%
+  mutate(observation_concept_id = as.integer(observation_concept_id)) %>%
+  rename(concept_id = observation_concept_id) %>%
+  distinct() %>%
+  tidyr::drop_na()
+
+rm(observation)
+
+measurement_concepts <- measurement %>%
+  select(measurement_concept_id) %>%
+  mutate(measurement_concept_id = as.integer(measurement_concept_id)) %>%
+  rename(concept_id = measurement_concept_id) %>%
+  distinct() %>%
+  tidyr::drop_na()
+
+visit_concepts <- visit_occurrence %>%
+  select(visit_concept_id) %>%
+  mutate(visit_concept_id = as.integer(visit_concept_id)) %>%
+  rename(concept_id = visit_concept_id) %>%
+  distinct() %>%
+  tidyr::drop_na()
+
+all_concepts = rbind(cond_concepts,
+                     proc_concepts,
+                     drug_concepts,
+                     observation_concepts,
+                     measurement_concepts,
+                     visit_concepts)
+
+
 # Pull matching concepts from omopcept
-concept_conditions <- omopcept::omop_concept() %>%
-  filter(concept_id %in% cond_concepts$condition_concept_id) %>%
+concepts <- omopcept::omop_concept() %>%
+  filter(concept_id %in% all_concepts$concept_id) %>%
   collect()
+
+
 
 # ============================
 # 4. Write everything back to CSV
 # ============================
 
-dir.create("processed", showWarnings = FALSE)
+library(fs)
 
-write_csv(person,               "processed/person.csv")
-write_csv(condition_occurrence, "processed/condition_occurrence.csv")
-write_csv(procedure_occurrence, "processed/procedure_occurrence.csv")
-write_csv(measurement,          "processed/measurement.csv")
-write_csv(visit_occurrence,     "processed/visit_occurrence.csv")
-write_csv(drug_exposure,        "processed/drug_exposure.csv")
-write_csv(observation,          "processed/observation.csv")
-write_csv(concept,              "processed/concept.csv")
-write_csv(concept_relationship, "processed/concept_relationship.csv")
+dir_create("OMOP")
 
-write_csv(cond_concepts,        "processed/cond_concepts.csv")
-write_csv(concept_conditions,   "processed/concept_conditions.csv")
+files <- dir_ls("raw/mimic_IV_omop_data_csv", type = "file") 
+
+# Move them
+file_move(files, "OMOP")
+
+# Write better concepts table
+write_csv(concepts,               "OMOP/concepts.csv")
+
+
 
 cat("All processed CSVs written to /processed\n")
 ## this program creates the foundational environment in binder
